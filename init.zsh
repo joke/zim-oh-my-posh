52bcb4065c23f3bd858dcb02b8618c52898333d7
Joke de Buhr <joke@xckk.de>
2021-10-15 20:20:06 +0200

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,21 @@
+(( ${+commands[oh-my-posh]} || ${+commands[asdf]} && ${+functions[_direnv_hook]} )) && () {
+
+  local command=${commands[oh-my-posh]:-"$(${commands[asdf]} which oh-my-posh 2> /dev/null)"}
+  [[ -z $command ]] && return 1
+
+  # generating init file
+  local initfile=$1/oh-my-posh-init.zsh
+  if [[ ! -e $initfile || $initfile -ot $command ]]; then
+    $command init zsh --print-full-init >| $initfile
+    zcompile -UR $initfile
+  fi
+
+  # generating completions
+  local compfile=$1/functions/_oh-my-posh
+  if [[ ! -e $compfile || $compfile -ot $command ]]; then
+    $command completions zsh >| $compfile
+    print -u2 -PR "* Detected a new version 'oh-my-posh'. Regenerated completions."
+  fi
+
+  source $initfile
+} ${0:h}
